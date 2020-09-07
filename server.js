47a9eb8f895893165aa82803e6f1836e66ca90f8
Dylan Best <best.dylan15@gmail.com>
2020-09-06 21:35:07 -0400

    added middleware and ids to json obj

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Requirements
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 // Sets up the Express App
 var app = express();
@@ -9,8 +11,34 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // ROUTES
+
+// api get request
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", function (err, data) {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    })
+})
+
+// api post request
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile("./db/db.json", function (err, data) {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+            if (err) throw err;
+            res.json(notes);
+        })
+    })
+})
+
+
 
 // html routes
 app.get("/notes", function (req, res) {
